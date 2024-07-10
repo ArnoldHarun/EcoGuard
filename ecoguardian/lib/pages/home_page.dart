f5c9390882d5d1c493f955c0d6aece1a1e0dc5cd
OYOKA DANIEL <oyokadaniel@gmail.com>
2024-07-10 07:37:32 -0700

    modified the login and sigup pages together with home page

--- ecoguardian/lib/pages/home_page.dart
+++ ecoguardian/lib/pages/home_page.dart
@@ -1,108 +1,397 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    HomeTab(),
+    ReportIssueTab(),
+    LearnTab(),
+    NotificationsTab(),
+    ProfileTab(),
+    EmergencyContactsTab(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home'),
+        title: const Text(
+          'Eco Guardian',
+          style: TextStyle(
+            fontSize: 24.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.green,
+        elevation: 0.0,
       ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            _buildFeatureCard(
-              context,
-              'Report an Issue',
-              'Report environmental issues to authorities',
-              Icons.report_problem_rounded,
-              '/report',
-              Colors.red,
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.green,
+              ),
+              child: Text(
+                'Navigation',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
             ),
-            const SizedBox(height: 20.0),
-            _buildFeatureCard(
-              context,
-              'Learn About Conservation',
-              'Discover tips and practices for environmental conservation',
-              Icons.eco_rounded,
-              '/education',
-              Colors.green,
+            ListTile(
+              leading: const Icon(Icons.home),
+              title: const Text('Home'),
+              onTap: () {
+                _onItemTapped(0);
+                Navigator.pop(context);
+              },
             ),
-            const SizedBox(height: 20.0),
-            _buildFeatureCard(
-              context,
-              'Notifications',
-              'Get updates and alerts on reported issues',
-              Icons.notifications_rounded,
-              '/notifications',
-              Colors.blue,
+            ListTile(
+              leading: const Icon(Icons.report),
+              title: const Text('Report Issue'),
+              onTap: () {
+                _onItemTapped(1);
+                Navigator.pop(context);
+              },
             ),
-            const SizedBox(height: 20.0),
-            _buildFeatureCard(
-              context,
-              'My Profile',
-              'View and manage your profile settings',
-              Icons.person_rounded,
-              '/profile',
-              Colors.purple,
+            ListTile(
+              leading: const Icon(Icons.info),
+              title: const Text('Learn'),
+              onTap: () {
+                _onItemTapped(2);
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.notifications),
+              title: const Text('Notifications'),
+              onTap: () {
+                _onItemTapped(3);
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.account_circle),
+              title: const Text('Profile'),
+              onTap: () {
+                _onItemTapped(4);
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.contact_phone),
+              title: const Text('Emergency Contacts'),
+              onTap: () {
+                _onItemTapped(5);
+                Navigator.pop(context);
+              },
             ),
           ],
         ),
       ),
+      body: _widgetOptions.elementAt(_selectedIndex),
+    );
+  }
+}
+
+class HomeTab extends StatelessWidget {
+  const HomeTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          const Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Text(
+              'Save your environment to save the future',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+          _buildCarouselSlider(),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                _buildCard(
+                  icon: Icons.report,
+                  title: 'Report Environmental Issues',
+                  description: 'Notify authorities about environmental issues.',
+                  onTap: () {
+                    Navigator.pushNamed(context, '/report');
+                  },
+                ),
+                _buildCard(
+                  icon: Icons.info,
+                  title: 'Learn about Conservation',
+                  description:
+                      'Get information on how to conserve the environment.',
+                  onTap: () {
+                    Navigator.pushNamed(context, '/learn');
+                  },
+                ),
+                _buildCard(
+                  icon: Icons.notifications,
+                  title: 'Receive Notifications',
+                  description: 'Stay updated with the latest news and alerts.',
+                  onTap: () {
+                    Navigator.pushNamed(context, '/notifications');
+                  },
+                ),
+                _buildCard(
+                  icon: Icons.account_circle,
+                  title: 'Manage Profile',
+                  description: 'Update your profile and settings.',
+                  onTap: () {
+                    Navigator.pushNamed(context, '/profile');
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 
-  Widget _buildFeatureCard(BuildContext context, String title, String subtitle,
-      IconData iconData, String route, Color color) {
-    return GestureDetector(
-      onTap: () {
-        Navigator.pushNamed(context, route);
-      },
-      child: Card(
-        elevation: 4.0,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(15.0),
-        ),
-        child: Container(
-          padding: const EdgeInsets.all(16.0),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(15.0),
-            gradient: LinearGradient(
-              colors: [color.withOpacity(0.8), color.withOpacity(0.6)],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Icon(
-                iconData,
-                size: 36.0,
-                color: Colors.white,
+  Widget _buildCarouselSlider() {
+    List<String> images = [
+      'assets/image1.jpg',
+      'assets/image2.jpg',
+      'assets/image3.jpg',
+      'assets/image4.jpg',
+      'assets/image5.jpg',
+    ];
+
+    return CarouselSlider(
+      options: CarouselOptions(
+        height: 200.0,
+        enlargeCenterPage: true,
+        autoPlay: true,
+        autoPlayInterval: const Duration(seconds: 5),
+        aspectRatio: 2.0,
+      ),
+      items: images.map((image) {
+        return Builder(
+          builder: (BuildContext context) {
+            return Container(
+              width: MediaQuery.of(context).size.width,
+              margin: const EdgeInsets.symmetric(horizontal: 5.0),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10.0),
               ),
-              const SizedBox(height: 12.0),
-              Text(
-                title,
-                style: const TextStyle(
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10.0),
+                child: Image.asset(
+                  image,
+                  fit: BoxFit.cover,
                 ),
               ),
-              const SizedBox(height: 8.0),
-              Text(
-                subtitle,
-                style: const TextStyle(
-                  fontSize: 14.0,
-                  color: Colors.white,
+            );
+          },
+        );
+      }).toList(),
+    );
+  }
+
+  Widget _buildCard({
+    required IconData icon,
+    required String title,
+    required String description,
+    required VoidCallback onTap,
+  }) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Card(
+        margin: const EdgeInsets.only(bottom: 16.0),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        elevation: 4.0,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Row(
+            children: [
+              Icon(
+                icon,
+                size: 40.0,
+                color: Colors.green,
+              ),
+              const SizedBox(width: 16.0),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      title,
+                      style: const TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 4.0),
+                    Text(
+                      description,
+                      style: const TextStyle(
+                        fontSize: 14.0,
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class ReportIssueTab extends StatelessWidget {
+  const ReportIssueTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Report Environmental Issues',
+        style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+}
+
+class LearnTab extends StatelessWidget {
+  const LearnTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Learn about Conservation',
+        style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+}
+
+class NotificationsTab extends StatelessWidget {
+  const NotificationsTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Receive Notifications',
+        style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+}
+
+class ProfileTab extends StatelessWidget {
+  const ProfileTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Manage Profile',
+        style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+}
+
+class EmergencyContactsTab extends StatelessWidget {
+  const EmergencyContactsTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            'Emergency Contacts',
+            style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 16.0),
+          _buildContactCard(
+            'NEMA',
+            'National Environment Management Authority',
+            'Phone: +256 414 251064\nEmail: info@nema.go.ug',
+          ),
+          _buildContactCard(
+            'NWSC',
+            'National Water and Sewerage Corporation',
+            'Phone: +256 414 315800\nEmail: info@nwsc.co.ug',
+          ),
+          _buildContactCard(
+            'NFA',
+            'National Forestry Authority',
+            'Phone: +256 414 233167\nEmail: info@nfa.org.ug',
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildContactCard(String title, String subtitle, String contactInfo) {
+    return Card(
+      margin: const EdgeInsets.only(bottom: 16.0),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      elevation: 4.0,
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              title,
+              style: const TextStyle(
+                fontSize: 18.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 4.0),
+            Text(
+              subtitle,
+              style: const TextStyle(
+                fontSize: 14.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.grey,
+              ),
+            ),
+            const SizedBox(height: 8.0),
+            Text(
+              contactInfo,
+              style: const TextStyle(fontSize: 14.0),
+            ),
+          ],
         ),
       ),
     );
--- ecoguardian/lib/pages/signup_page.dart
+++ ecoguardian/lib/pages/signup_page.dart
@@ -7,39 +7,202 @@ class SignupPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Sign Up'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const TextField(
-              decoration: InputDecoration(labelText: 'Email'),
-            ),
-            const TextField(
-              decoration: InputDecoration(labelText: 'Password'),
-              obscureText: true,
-            ),
-            const TextField(
-              decoration: InputDecoration(labelText: 'Confirm Password'),
-              obscureText: true,
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushReplacementNamed(context, '/login');
-              },
-              child: const Text('Sign Up'),
-            ),
-            TextButton(
-              onPressed: () {
-                Navigator.pushReplacementNamed(context, '/login');
-              },
-              child: const Text('Already have an account? Log in'),
-            ),
-          ],
+        title: const Text(
+          'Sign Up',
+          style: TextStyle(
+            fontSize: 24.0,
+            fontWeight: FontWeight.bold,
+          ),
         ),
+        centerTitle: true,
+        backgroundColor: const Color(0xff0ACF83),
+        elevation: 0.0,
+      ),
+      body: Stack(
+        children: [
+          const Image(
+            fit: BoxFit.cover,
+            height: double.infinity,
+            width: double.infinity,
+            image: AssetImage('assets/image6.jpg'),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Colors.black.withOpacity(0.7),
+                  Colors.black.withOpacity(0.3),
+                ],
+                begin: Alignment.bottomCenter,
+                end: Alignment.topCenter,
+              ),
+            ),
+          ),
+          Center(
+            child: SingleChildScrollView(
+              padding: const EdgeInsets.symmetric(horizontal: 30.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Join Eco Guardian',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  const Text(
+                    'Sign up to help protect the environment and stay updated on conservation efforts.',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                    ),
+                  ),
+                  const SizedBox(height: 30),
+                  const TextField(
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 15,
+                      fontFamily: 'Poppins',
+                      fontWeight: FontWeight.w400,
+                    ),
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Colors.white,
+                      labelText: 'Email',
+                      labelStyle: TextStyle(
+                        color: Color(0xff0ACF83),
+                        fontSize: 15,
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.w600,
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                        borderSide: BorderSide(
+                          width: 1,
+                          color: Color(0xff0ACF83),
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                        borderSide: BorderSide(
+                          width: 1,
+                          color: Color(0xff0ACF83),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  const TextField(
+                    obscureText: true,
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 15,
+                      fontFamily: 'Poppins',
+                      fontWeight: FontWeight.w400,
+                    ),
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Colors.white,
+                      labelText: 'Password',
+                      labelStyle: TextStyle(
+                        color: Color(0xff0ACF83),
+                        fontSize: 15,
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.w600,
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                        borderSide: BorderSide(
+                          width: 1,
+                          color: Color(0xff0ACF83),
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                        borderSide: BorderSide(
+                          width: 1,
+                          color: Color(0xff0ACF83),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  const TextField(
+                    obscureText: true,
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 15,
+                      fontFamily: 'Poppins',
+                      fontWeight: FontWeight.w400,
+                    ),
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Colors.white,
+                      labelText: 'Confirm Password',
+                      labelStyle: TextStyle(
+                        color: Color(0xff0ACF83),
+                        fontSize: 15,
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.w600,
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                        borderSide: BorderSide(
+                          width: 1,
+                          color: Color(0xff0ACF83),
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                        borderSide: BorderSide(
+                          width: 1,
+                          color: Color(0xff0ACF83),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 30),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushReplacementNamed(context, '/login');
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xff0ACF83),
+                      minimumSize: const Size(double.infinity, 50),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    child: const Text(
+                      'Sign Up',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pushReplacementNamed(context, '/login');
+                    },
+                    child: const Text(
+                      'Already have an account? Log in',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
