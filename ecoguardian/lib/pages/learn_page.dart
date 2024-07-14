f2d62d594b5c524b7efa5fa2aeb30116b83beb9e
OYOKA DANIEL <oyokadaniel@gmail.com>
2024-07-14 05:25:24 -0700

    made the learn about conservation page to have better looking cards

--- ecoguardian/lib/pages/learn_page.dart
+++ ecoguardian/lib/pages/learn_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
-import 'package:animated_text_kit/animated_text_kit.dart';
-import 'package:youtube_player_flutter/youtube_player_flutter.dart';
+import 'package:youtube_player_iframe/youtube_player_iframe.dart';
 
 class LearnPage extends StatefulWidget {
   const LearnPage({super.key});
@@ -11,24 +10,34 @@ class LearnPage extends StatefulWidget {
 }
 
 class _LearnPageState extends State<LearnPage> {
-  YoutubePlayerController? _controller;
+  late YoutubePlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = YoutubePlayerController(
+      initialVideoId: '',
+      params: const YoutubePlayerParams(
+        autoPlay: false,
+        mute: false,
+      ),
+    );
+  }
 
   void _initializePlayer(String videoUrl) {
-    final videoId = YoutubePlayer.convertUrlToId(videoUrl);
+    final videoId = YoutubePlayerController.convertUrlToId(videoUrl);
     if (videoId != null) {
-      _controller = YoutubePlayerController(
-        initialVideoId: videoId,
-        flags: const YoutubePlayerFlags(
-          autoPlay: true,
-          mute: false,
-        ),
-      );
+      setState(() {
+        _controller.load(videoId);
+      });
+    } else {
+      print('Invalid video URL: $videoUrl');
     }
   }
 
   @override
   void dispose() {
-    _controller?.dispose();
+    _controller.close();
     super.dispose();
   }
 
@@ -43,29 +52,87 @@ class _LearnPageState extends State<LearnPage> {
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: AnimationLimiter(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+            child: GridView.count(
+              physics: const NeverScrollableScrollPhysics(),
+              shrinkWrap: true,
+              crossAxisCount: 2,
               children: [
-                _buildSectionTitle('Introduction to Conservation'),
-                const SizedBox(height: 8.0),
-                _buildAnimatedText(
-                  'Conservation is the practice of protecting the natural environment for the benefit of all living organisms. It includes preserving biodiversity, restoring habitats, and reducing pollution.',
+                _buildInfoCard(
+                  'Reduce, Reuse, Recycle',
+                  'Minimize waste by reducing, reusing, and recycling materials.',
+                  'assets/image8.jpg',
+                  () => _showDetails(
+                    'Reduce, Reuse, Recycle',
+                    'This principle aims to minimize waste, protect the environment, and conserve resources.',
+                    'assets/image10.jpg',
+                  ),
                 ),
-                const SizedBox(height: 16.0),
-                _buildSectionTitle('How to Conserve the Environment'),
-                const SizedBox(height: 8.0),
-                ..._buildInfoCards(),
-                const SizedBox(height: 16.0),
-                _buildSectionTitle('Environmental Conservation in Uganda'),
-                const SizedBox(height: 8.0),
-                _buildAnimatedText(
-                  'Uganda is rich in biodiversity and natural resources. Here are some ways to help conserve the environment in Uganda:',
+                _buildInfoCard(
+                  'Plant Trees',
+                  'Trees provide oxygen and absorb CO2. Join tree planting initiatives!',
+                  'assets/image9.jpg',
+                  () => _showDetails(
+                    'Plant Trees',
+                    'Tree planting enhances biodiversity and combats climate change.',
+                    'assets/image13.jpg',
+                  ),
                 ),
-                ..._buildUgandaInfoCards(),
-                const SizedBox(height: 16.0),
-                _buildSectionTitle('Educational Videos'),
-                const SizedBox(height: 8.0),
-                ..._buildVideoCards(),
+                _buildInfoCard(
+                  'Support Conservation Organizations',
+                  'Volunteer or donate to groups dedicated to protecting the environment.',
+                  'assets/image12.jpg',
+                  () => _showDetails(
+                    'Support Organizations',
+                    'Your contribution helps in the fight against environmental degradation.',
+                    'assets/image7.jpg',
+                  ),
+                ),
+                _buildInfoCard(
+                  'Educational Videos',
+                  'Watch videos to learn more about conservation efforts.',
+                  'assets/image8.jpg',
+                  () => _initializePlayer('https://youtu.be/HxsbXVlTAmo'),
+                ),
+                _buildInfoCard(
+                  'Combat Soil Erosion',
+                  'Learn about techniques to prevent soil erosion, such as planting cover crops.',
+                  'assets/image2.jpg',
+                  () => _showDetails(
+                    'Combat Soil Erosion',
+                    'Cover crops can help improve soil health and prevent erosion.',
+                    'assets/image5.jpg',
+                  ),
+                ),
+                _buildInfoCard(
+                  'Avoid Plastic Use',
+                  'Reduce plastic usage by opting for reusable bags and containers.',
+                  'assets/image2.jpg',
+                  () => _showDetails(
+                    'Avoid Plastic Use',
+                    'Using reusable products can significantly reduce waste and protect wildlife.',
+                    'assets/image3.jpg',
+                  ),
+                ),
+                _buildInfoCard(
+                  'Educate Others',
+                  'Share information about conservation with friends and family.',
+                  'assets/image12.jpg',
+                  () => _showDetails(
+                    'Educate Others',
+                    'Spreading awareness is crucial for community engagement in conservation.',
+                    'assets/image9.jpg',
+                  ),
+                ),
+                _buildInfoCard(
+                  'Reduce Carbon Footprint',
+                  'Use public transport, bike, or walk to reduce greenhouse gas emissions.',
+                  'assets/image1.jpg',
+                  () => _showDetails(
+                    'Reduce Carbon Footprint',
+                    'Small changes in transportation habits can lead to significant reductions in carbon emissions.',
+                    'assets/image3.jpg',
+                  ),
+                ),
               ],
             ),
           ),
@@ -74,157 +141,73 @@ class _LearnPageState extends State<LearnPage> {
     );
   }
 
-  Widget _buildSectionTitle(String title) {
-    return Text(
-      title,
-      style: const TextStyle(
-        fontSize: 24.0,
-        fontWeight: FontWeight.bold,
-        color: Colors.green,
-      ),
-    );
-  }
-
-  Widget _buildAnimatedText(String text) {
-    return AnimatedTextKit(
-      animatedTexts: [
-        TypewriterAnimatedText(
-          text,
-          textStyle: const TextStyle(fontSize: 16.0),
-          speed: const Duration(milliseconds: 50),
-        ),
-      ],
-      totalRepeatCount: 1,
-    );
-  }
-
-  List<Widget> _buildInfoCards() {
-    return [
-      _buildInfoCard(
-        'Reduce, Reuse, Recycle',
-        'Minimize waste by reducing what you use, reusing items, and recycling materials whenever possible.',
-        'assets/image8.jpg',
-      ),
-      _buildInfoCard(
-        'Plant Trees',
-        'Trees absorb carbon dioxide and provide oxygen. Participate in tree planting activities or start your own tree planting project.',
-        'assets/image9.jpg',
-      ),
-    ];
-  }
-
-  List<Widget> _buildUgandaInfoCards() {
-    return [
-      _buildInfoCard(
-        'Support Conservation Organizations',
-        'Donate to or volunteer with organizations working on conservation projects in Uganda.',
-        'assets/image12.jpg',
-      ),
-    ];
-  }
-
-  List<Widget> _buildVideoCards() {
-    return [
-      _buildVideoCard(
-        'Tree Planting in Uganda',
-        'https://youtu.be/0mPurj3WS6M?si=bSqq0jT25UNPegnp',
-      ),
-    ];
-  }
-
-  Widget _buildInfoCard(String title, String description, String imagePath) {
-    return AnimationConfiguration.staggeredList(
-      position: 0,
-      duration: const Duration(milliseconds: 375),
-      child: ScaleAnimation(
-        child: Card(
-          margin: const EdgeInsets.symmetric(vertical: 8.0),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10.0),
-          ),
-          elevation: 4.0,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              ClipRRect(
-                borderRadius: const BorderRadius.vertical(top: Radius.circular(10.0)),
-                child: Image.asset(imagePath, fit: BoxFit.cover),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      title,
-                      style: const TextStyle(
-                        fontSize: 18.0,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    const SizedBox(height: 8.0),
-                    Text(
-                      description,
-                      style: const TextStyle(fontSize: 16.0),
-                    ),
-                  ],
+  Widget _buildInfoCard(
+      String title, String description, String imagePath, Function onTap) {
+    return GestureDetector(
+      onTap: () => onTap(),
+      child: AnimationConfiguration.staggeredGrid(
+        position: 0,
+        duration: const Duration(milliseconds: 375),
+        columnCount: 2, // Add this line
+        child: ScaleAnimation(
+          child: Card(
+            margin: const EdgeInsets.all(8.0),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            elevation: 4.0,
+            child: Column(
+              children: [
+                ClipRRect(
+                  borderRadius:
+                      const BorderRadius.vertical(top: Radius.circular(10.0)),
+                  child: Image.asset(imagePath, fit: BoxFit.cover, height: 100),
                 ),
-              ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    title,
+                    style: const TextStyle(
+                        fontSize: 18.0, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    description,
+                    style: const TextStyle(fontSize: 14.0),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _showDetails(String title, String description, String imagePath) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text(title),
+        content: SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.min, // Important for preventing overflow
+            children: [
+              Image.asset(imagePath),
+              const SizedBox(height: 10),
+              Text(description),
             ],
           ),
         ),
-      ),
-    );
-  }
-
-  Widget _buildVideoCard(String title, String videoUrl) {
-    return AnimationConfiguration.staggeredList(
-      position: 0,
-      duration: const Duration(milliseconds: 375),
-      child: ScaleAnimation(
-        child: Card(
-          margin: const EdgeInsets.symmetric(vertical: 8.0),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10.0),
+        actions: [
+          TextButton(
+            child: const Text('Close'),
+            onPressed: () => Navigator.of(context).pop(),
           ),
-          elevation: 4.0,
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  title,
-                  style: const TextStyle(
-                    fontSize: 18.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                const SizedBox(height: 8.0),
-                AspectRatio(
-                  aspectRatio: 16 / 9,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(10.0),
-                    child: _controller != null
-                        ? YoutubePlayer(
-                            controller: _controller!,
-                            showVideoProgressIndicator: true,
-                          )
-                        : Container(color: Colors.black), // Placeholder
-                  ),
-                ),
-                IconButton(
-                  icon: const Icon(Icons.play_circle_fill,
-                      size: 48.0, color: Colors.green),
-                  onPressed: () {
-                    _initializePlayer(videoUrl);
-                    setState(() {}); // Refresh to show video player
-                  },
-                ),
-              ],
-            ),
-          ),
-        ),
+        ],
       ),
     );
   }
